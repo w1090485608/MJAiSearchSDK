eb1976f88624bc01b28cdec1a6ceafd9084d6119
袁兴杨 <xingyang.yuan@dataenlighten.com>
2018-11-07 13:47:28 +0800

    111

---
+++ MJAiSearchDemo/Pods/MJAiSearchSDK/MJOrderServiceLib.framework/Headers/MJOrderServiceLib.h
@@ -0,0 +1,19 @@
+//
+//  MJOrderServiceLib.h
+//  MJOrderServiceLib
+//
+//  Created by 袁兴杨 on 2018/3/20.
+//  Copyright © 2018年 袁兴杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJOrderServiceLib.
+FOUNDATION_EXPORT double MJOrderServiceLibVersionNumber;
+
+//! Project version string for MJOrderServiceLib.
+FOUNDATION_EXPORT const unsigned char MJOrderServiceLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJOrderServiceLib/PublicHeader.h>
+
+#import <MJOrderServiceLib/MJOrderService.h>
